ea3923e4728a3c163bdd1d79f7cb033b2ed7a823
gayetekin <100995170+gayetekin@users.noreply.github.com>
2023-05-15 23:14:40 +0300

    added questions section to index.html, style.css and main.js files

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,3 +37,34 @@ function scrollHeader(){
     if(this.scrollY >= 80) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
+
+
+/* --- questions accordion --- */
+const accordionItems = document.querySelectorAll('.questions_item')
+
+accordionItems.forEach((item) =>{
+    const accordionHeader = item.querySelector('.questions_header')
+
+    accordionHeader.addEventListener('click', () =>{
+        const openItem = document.querySelector('.accordion-open')
+
+        toggleItem(item)
+        // when it opens new content, the previous one closes
+        if(openItem && openItem!== item){
+            toggleItem(openItem)
+        }
+    })
+})
+
+const toggleItem = (item) =>{
+    const accordionContent = item.querySelector('.questions_content')
+
+    if(item.classList.contains('accordion-open')){
+        accordionContent.removeAttribute('style')
+        item.classList.remove('accordion-open')
+    }else{
+        accordionContent.style.height = accordionContent.scrollHeight + 'px'
+        item.classList.add('accordion-open')
+    }
+
+}
